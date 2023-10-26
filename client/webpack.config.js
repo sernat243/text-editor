bc9bbaf68030d75e18a3022d715ca3f1ab945f09
Gabriel <sernat243@gmail.com>
2023-10-25 22:53:50 -0600

    added image handling to webpack config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -43,8 +43,12 @@ module.exports = () => {
     module: {
       rules: [
         {
-          test: /\.css$/,
+          test: /\.css$/i,
           use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: "asset/resource",
         },
         {
           test: /\.js$/,
